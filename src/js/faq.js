25309662d5fe67c7dce9af41821ae1018ce1a95b
Anastasiia Posliednichenko <anastasia29po@gmail.com>
2025-04-06 03:39:14 +0200

    update faq section

--- src/js/faq.js
+++ src/js/faq.js
@@ -3,20 +3,12 @@ import "accordion-js/dist/accordion.min.css";
 
 document.addEventListener("DOMContentLoaded", function() {
   new Accordion('.accordion', {
-    duration: 300,    
-    showMultiple: true, 
-    openOnInit: [],    
-  });
-  
-  document.querySelectorAll('.faq-btn').forEach(button => {
-    button.addEventListener('click', (event) => {
-      const faqItem = event.target.closest('.faq-item');
-      const text = faqItem.querySelector('.faq-item-text');
-      const icon = button.querySelector('.faq-icon');
-
-      text.style.display = (text.style.display === 'block') ? 'none' : 'block';
-
-      icon.classList.toggle('rotated');
-    });
+    duration: 300,
+    showMultiple: true,
+    elementClass: 'faq-ac',      
+    triggerClass: 'faq-ac-trigger', 
+    panelClass: 'faq-ac-panel',    
+    activeClass: 'is-active'
   });
 });
+
