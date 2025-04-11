7e81f4b82a757bc91ebb5ac18985cbd7e17d8736
PodletskyiDenys <denus.podletskiy@gmail.com>
2025-04-11 15:09:13 +0300

    return js

--- src/js/reviews_slider.js
+++ src/js/reviews_slider.js
@@ -6,62 +6,39 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 document.addEventListener('DOMContentLoaded', function () {
-  const reviewsPerSection = determineReviewsCount();
-  const totalReviewsToLoad = reviewsPerSection * 6; // 6 секцій
-  loadReviews(totalReviewsToLoad);
-
-  const swiperContainer = document.querySelector('.reviews-swiper');
-  swiperContainer.addEventListener('scroll', handleScroll);
+  loadReviews();
 });
 
-function determineReviewsCount() {
-  if (window.matchMedia('(max-width: 767px)').matches) {
-    return 1;
-  } else if (
-    window.matchMedia('(min-width: 768px) and (max-width: 1439px)').matches
-  ) {
-    return 2;
-  } else {
-    return 5;
-  }
-}
-
-let startIndex = 0;
-let allReviewsLoaded = false;
-
-async function loadReviews(reviewsToLoad) {
+async function loadReviews() {
   const swiperWrapper = document.querySelector('.reviews-list');
+
   try {
     const response = await axios.get(
       'https://portfolio-js.b.goit.study/api/reviews'
     );
-    const reviews = response.data.slice(startIndex, startIndex + reviewsToLoad);
-
-    if (reviews.length === 0) {
-      allReviewsLoaded = true;
-      return;
-    }
+    const reviews = response.data;
 
     const reviewsAdd = reviews
       .map(
         review => `
         <div class="swiper-slide swiper-slide-reviews">
-          <img class="review-img" src="${review.avatar_url}" alt="${review.author}" loading="lazy" />
+          <img class="review-img" src="${review.avatar_url}" alt="${review.author}" />
           <p class="review-name">${review.author}</p>
-          <p class="review-text">${review.review}</p>
+          <p class="review-text">${review.review}</p>  
         </div>
       `
       )
       .join('');
 
     swiperWrapper.insertAdjacentHTML('beforeend', reviewsAdd);
-    initSwiper();
 
-    startIndex += reviewsToLoad;
+    initSwiper();
   } catch (error) {
     swiperWrapper.innerHTML =
       '<div class="reviews-error">Failed to load reviews. Please try again later.</div>';
+
     const errorElement = document.querySelector('.reviews-error');
+
     const observer = new IntersectionObserver(
       (entries, observer) => {
         entries.forEach(entry => {
@@ -76,28 +53,16 @@ async function loadReviews(reviewsToLoad) {
       },
       { threshold: 0.5 }
     );
+
     observer.observe(errorElement);
   }
 }
 
-function handleScroll() {
-  const swiperContainer = document.querySelector('.reviews-swiper');
-  const scrollPosition =
-    swiperContainer.scrollLeft + swiperContainer.offsetWidth;
-  const totalWidth = swiperContainer.scrollWidth;
-
-  if (scrollPosition >= totalWidth - 50 && !allReviewsLoaded) {
-    const reviewsToLoad = determineReviewsCount();
-    loadReviews(reviewsToLoad);
-  }
-}
-
 function initSwiper() {
-  new Swiper('.reviews-swiper', {
+  const swiper = new Swiper('.reviews-swiper', {
     modules: [Navigation, Pagination, Keyboard],
     watchOverflow: true,
     slidesPerView: 1,
-    slidesPerGroup: 1,
     navigation: {
       nextEl: '.button-next',
       prevEl: '.button-prev',
@@ -114,7 +79,6 @@ function initSwiper() {
       },
       1440: {
         slidesPerView: 4,
-        slidesPerGroup: 1,
         spaceBetween: 16,
       },
     },
